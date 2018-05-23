1cc41f82f92ac98ae44f1dd41f7080fdbcf28852
Alexandra Crisan <36537392+alexa26cris@users.noreply.github.com>
2018-05-23 10:48:20 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var allEnemies = [];
+var key = null;
+
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.x = x;
+    this.y = y;
     this.sprite = 'images/enemy-bug.png';
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +20,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+        this.x += this.speed * dt;
+    if(this.x >=505) {
+      this.x = 0;
+    }
+    this.checkCrash(this)
 };
 
 // Draw the enemy on the screen, required method for game
@@ -29,7 +40,21 @@ Enemy.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var Player = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-cat-girl.png';
+    this.speed = speed;
+  };
+  Player.prototype.render = function() {
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  };
+  Player.prototype.update = function(dt) {};
+  Player.prototype.handleInput = function(keyPress) {
+    if (keyPress !== undefined && player.moveAllowed === true) {
+    key = keyPress;
+}
+};
 
 
 // This listens for key presses and sends the keys to your
